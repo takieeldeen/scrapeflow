70ba8f33847cf74bff86cc4d884787a3e101dfff
takieeldeen <takie.eldeen1998@gmail.com>
2025-12-27 20:30:28 +0200

    inital commit

--- app/workflow/_components/topbar/ExecuteBtn.tsx
+++ app/workflow/_components/topbar/ExecuteBtn.tsx
@@ -4,28 +4,18 @@ import { Button } from "@/components/ui/button";
 import { useMutation } from "@tanstack/react-query";
 import { useReactFlow } from "@xyflow/react";
 import { PlayIcon } from "lucide-react";
-<<<<<<< HEAD
 import { useRouter } from "next/navigation";
-=======
->>>>>>> 158dafbbf439335d3816bfbf6060297473f9dc66
 import React, { useCallback } from "react";
 import { toast } from "sonner";
 
 function ExecuteBtn({ workflowId }: { workflowId: string }) {
   const { toObject } = useReactFlow();
-<<<<<<< HEAD
   const router = useRouter();
   const { mutate, isPending } = useMutation({
     mutationFn: RunWorkflow,
     onSuccess: ({ executionId }) => {
       toast.success("Execution Started", { id: "flow-execution" });
       router.push(`/workflow/runs/${workflowId}/${executionId}`);
-=======
-  const { mutate, isPending } = useMutation({
-    mutationFn: RunWorkflow,
-    onSuccess: () => {
-      toast.success("Execution Started", { id: "flow-execution" });
->>>>>>> 158dafbbf439335d3816bfbf6060297473f9dc66
     },
     onError: () => {
       toast.error("Execution Failed", { id: "flow-execution" });
